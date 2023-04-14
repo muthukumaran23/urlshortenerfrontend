8fd2bc10da32e9e4b13db0ae698a1ed139354216
muthukumaran23 <tvm.muthukumaran007@gmail.com>
2023-04-14 00:08:22 -0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const env = {
+    // api : "http://localhost:4000/auth",
+    // api1 : "http://localhost:4000/api",
+    // redirect : "http://localhost:4000/api/shortUrl"
+
+    api : "https://urlshortener-backend-eight.vercel.app/auth",
+    api1 : "https://urlshortener-backend-eight.vercel.app/api",
+    redirect : "https://urlshortener-backend-eight.vercel.app/api/shortUrl"
+    
+    
+}
